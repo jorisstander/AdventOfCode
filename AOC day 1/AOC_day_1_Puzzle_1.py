497eaabf1421073f677ebb9ce2f9e453dda69a6b
Joris Stander <joris.stander@hotmail.com>
2022-12-02 15:11:21 +0100

    removed useless variable
    removed useless variable

--- AOC day 1/AOC_day_1_Puzzle_1.py
+++ AOC day 1/AOC_day_1_Puzzle_1.py
@@ -2,13 +2,12 @@ with open('input_puzzle1.txt') as f:
     lines = f.readlines()
     CalorieList = []
     amount = 0
-    counterIndex = 0
     for i in lines:
         if i != '\n':
             amount += int(i)
         else:
-            counterIndex += 1
             CalorieList.append(amount)
             amount = 0
-    
+
+    #answer puzzle 1 day 1
     print(max(CalorieList))
--- AOC day 1/AOC_day_1_Puzzle_2.py
+++ AOC day 1/AOC_day_1_Puzzle_2.py
@@ -2,12 +2,10 @@ with open('input_puzzle1.txt') as f:
     lines = f.readlines()
     CalorieList = []
     amount = 0
-    counterIndex = 0
     for i in lines:
         if i != '\n':
             amount += int(i)
         else:
-            counterIndex += 1
             CalorieList.append(amount)
             amount = 0
     
